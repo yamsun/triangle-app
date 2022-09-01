0e967f7ec86efbd3ed5004fae7605dbfe60dc55a
Shyam Sundar <ssjha360@gmail.com>
2022-09-02 00:38:21 +0530

    ui improvements

--- quiz.js
+++ quiz.js
@@ -1,29 +1,30 @@
-const quizForm = document.querySelector('.quiz-form');
-const submitAnswerBtn = document.querySelector('#submit-answer-btn');
-const output = document.querySelector('#output');
+const quizForm = document.querySelector(".quiz-form");
+const submitAnswerBtn = document.querySelector("#submit-answer-btn");
+const output = document.querySelector("#output");
 
-const quizContainers = document.querySelectorAll('.question-container');
+const quizContainers = document.querySelectorAll(".question-container");
 
-const correctAnswers = ["90°", "right angled", "yes", "no", "yes", "isosceles" ];
+const correctAnswers = ["90°", "right angled", "yes", "no", "yes", "isosceles"];
 
 function calculateScore() {
-    let score = 0;
-    let index = 0;
-    const formResults = new FormData(quizForm);
-    
-    for(let value of formResults.values()){
-        if(value === correctAnswers[index]){
-            score = score + 1;
-            quizContainers[index].style.backgroundColor = "#C5EFCB";
-        }
-        else{
-            quizContainers[index].style.backgroundColor = "#FBDCE2";
-        }
-        index = index + 1;
+  let score = 0;
+  let index = 0;
+  const formResults = new FormData(quizForm);
+  console.log(formResults.values());
+
+  for (let value of formResults.values()) {
+    console.log(index + 1);
+    console.log(value);
+    if (value === correctAnswers[index]) {
+      score = score + 1;
+      quizContainers[index].style.backgroundColor = "#C5EFCB";
+    } else {
+      quizContainers[index].style.backgroundColor = "#FBDCE2";
     }
-    console.log(score);
-    output.innerText = "Your score is " + score + " / 6";
-    
+    index = index + 1;
+  }
+  console.log(score);
+  output.innerHTML = "You Scored " + "<b>" + score + " / 6" + "</b>";
 }
 
-submitAnswerBtn.addEventListener("click", calculateScore)
+submitAnswerBtn.addEventListener("click", calculateScore);
